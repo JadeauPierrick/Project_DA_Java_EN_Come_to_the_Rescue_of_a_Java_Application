7b98bfedd17744146b02bad5f300fc541d1950a1
Pierrick Jadeau <jadeau.pierrick@gmail.com>
2021-08-28 16:44:18 +0200

    Finalisation du code de chaque classe

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,27 +1,17 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+
 
 public class AnalyticsCounter {
 
 	public static void main(String[] args) throws IOException {
 
-		ISymptomReader newRead = new ReadSymptomDataFromFile("/Users/jacquesgrellier/Desktop/Formation Développeur JAVA/deuxieme_projet/Project02Eclipse/symptoms.txt");
+		ISymptomReader newRead = new ReadSymptomDataFromFile("symptoms.txt");
 		CountSymptomData newCount = new CountSymptomData(newRead);
 		DataWriterOnFile newData = new DataWriterOnFile("result.out");
 		newData.write(newCount.count());
 
 	}
+}
 
--- Project02Eclipse/src/com/hemebiotech/analytics/DataWriterOnFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/DataWriterOnFile.java
@@ -1,20 +1,38 @@
 package com.hemebiotech.analytics;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class DataWriterOnFile {
 
-    private String filepath;
+    private String fileway;
+    private Path path;
+    private String content;
 
-    public DataWriterOnFile(String filepath) {
-        this.filepath = filepath;
+    public DataWriterOnFile(String fileway) {
+        this.fileway = fileway;
     }
 
     public void write(List<String> data) {
 
+        try {
 
+            path = Paths.get(fileway);
+            content = "";
+            Files.write(path, String.format(content).getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+
+        }
 
     }
 
 }
-
